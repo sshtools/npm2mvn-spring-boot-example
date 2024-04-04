949ccb1e2c14247b15fc8d178a6dad4d2a1cdfa6
Lee Painter <lee@ludup.com>
2024-04-04 15:46:57 +0100

    Initial commit of working spring boot example.

---
+++ src/test/java/com/jadaptive/npm/Npm2mvnSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadaptive.npm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Npm2mvnSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
